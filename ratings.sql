d134231d49a61d2c6295f5f1a967e26f6c6566a0
KalluruS <>
2020-06-15 08:17:57 +0530

    Update rs-mysql/ Terraform Module

--- ratings.sql
+++ ratings.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE ratings
+CREATE DATABASE if not exists ratings
 DEFAULT CHARACTER SET 'utf8';
 
 USE ratings;
 
-CREATE TABLE ratings (
+CREATE TABLE if not exists ratings (
     sku varchar(80) NOT NULL,
     avg_rating DECIMAL(3, 2) NOT NULL,
     rating_count INT NOT NULL,
